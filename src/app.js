bf9cacae3033d5cf63901093d822fd2fb895222b
William Manoel dos Santos <wmsantos@icloud.com>
2020-07-02 21:32:45 -0300

    exercicio concluido

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,73 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  let likes = 0;
+  const item = {id:uuid(), title, url, techs, likes };
+
+  repositories.push(item);
+
+  return response.status(201).json(item);
+
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const {title, url, techs} = request.body;
+  const itemIndex = repositories.findIndex(item => item.id === id );
+  
+  if (itemIndex < 0 ){
+    return response.status(400).json({ error : 'repositorie not found'});
+  }
+
+  let { likes } = repositories.find(item => item.id === id );
+  
+  const item = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  };
+
+  repositories[itemIndex] = item;
+
+  return response.status(200).json(item);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  
+  const itemIndex = repositories.findIndex(item => item.id === id );
+ 
+  if (itemIndex < 0 ){
+    return response.status(400).json({ error : 'repositorie not found'});
+  }
+
+  repositories.splice(itemIndex, 1);
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  
+  const { id } = request.params;
+  
+  const item = repositories.find(item => item.id === id );
+  
+  if (!item){
+    return response.status(400).json({ error : 'repositorie not found'});
+  }
+
+  item.likes = item.likes + 1;
+
+  response.status(200).json(item);
 });
 
 module.exports = app;
